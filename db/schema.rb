82194f7890f0bad1adfac8abe941593cbd92d6ee
Tedis Rozenfelds <trozenfelds@gocardless.com>
2024-04-30 14:35:12 +0300

    active-record-migrations (git merge --squash)

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_11_064443) do
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "status"
+ActiveRecord::Schema[7.1].define(version: 2024_04_30_112727) do
+  create_table "customers_products", id: false, force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.integer "product_id", null: false
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.string "commenter"
-    t.text "body"
-    t.integer "article_id", null: false
+  create_table "products", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
-    t.index ["article_id"], name: "index_comments_on_article_id"
+    t.decimal "price"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
-  add_foreign_key "comments", "articles"
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "products", "users"
 end
